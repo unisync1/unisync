e0f96dbd1ddd41bc361918be2248fc213884cbc3
unisync1 <analogicacloud@gmail.com>
2021-11-12 16:01:03 +0000

    body

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,8 +2,22 @@ import React from "react";
 import Layout from "./src/components/layout";
 import "@fontsource/lato";
 
-
-
+export const onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <div key="netlify_form" id="netlify__form">
+      <form
+        name="contact"
+        data-netlify="true"
+        netlify-honeypot="bot-field"
+        hidden
+      >
+        <input type="text" name="name" />
+        <input type="email" name="email" />
+        <textarea name="message"></textarea>
+      </form>
+    </div>,
+  ]);
+};
 
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
--- src/components/LetsTalk.js
+++ src/components/LetsTalk.js
@@ -1,7 +1,5 @@
-import React from "react";
-import styled from "styled-components";
-import { navigate } from "gatsby";
-import Helmet from "react-helmet";
+import * as React from "react";
+import { navigate } from "gatsby-link";
 
 function encode(data) {
   return Object.keys(data)
@@ -9,13 +7,17 @@ function encode(data) {
     .join("&");
 }
 
-function LetsTalk() {
-  const [state, setState] = React.useState({});
-  const handleChange = (e) => {
-    setState({ ...state, [e.target.name]: e.target.value });
+export default class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isValidated: false };
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  handleSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
     fetch("/", {
@@ -23,66 +25,88 @@ function LetsTalk() {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({
         "form-name": form.getAttribute("name"),
-        ...state,
+        ...this.state,
       }),
     })
       .then(() => navigate(form.getAttribute("action")))
       .catch((error) => alert(error));
   };
-  return (
-    <Wrapper>
-      <Helmet>
-        <body className="netlify__form" />
-      </Helmet>
-      <form
-        name="contact"
-        method="POST"
-        action="/thank-you"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
-        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-        <input type="hidden" name="form-name" value="contact" />
-        <p hidden>
-          <label>
-            Don’t fill this out:{" "}
-            <input name="bot-field" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your name:
-            <br />
-            <input type="text" name="name" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your email:
-            <br />
-            <input type="email" name="email" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message:
-            <br />
-            <textarea name="message" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <button type="submit" onClick={handleSubmit}>
-            Send
-          </button>
-        </p>
-      </form>
-    </Wrapper>
-  );
+
+  render() {
+    return (
+        <section className="section">
+          <div className="container">
+            <div className="content">
+              <h1>Contact</h1>
+              <form
+                name="contact"
+                method="post"
+                action="/contact/thanks/"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                onSubmit={this.handleSubmit}
+              >
+                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                <input type="hidden" name="form-name" value="contact" />
+                <div hidden>
+                  <label>
+                    Don’t fill this out:{" "}
+                    <input name="bot-field" onChange={this.handleChange} />
+                  </label>
+                </div>
+                <div className="field">
+                  <label className="label" htmlFor={"name"}>
+                    Your name
+                  </label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type={"text"}
+                      name={"name"}
+                      onChange={this.handleChange}
+                      id={"name"}
+                      required={true}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label" htmlFor={"email"}>
+                    Email
+                  </label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type={"email"}
+                      name={"email"}
+                      onChange={this.handleChange}
+                      id={"email"}
+                      required={true}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label" htmlFor={"message"}>
+                    Message
+                  </label>
+                  <div className="control">
+                    <textarea
+                      className="textarea"
+                      name={"message"}
+                      onChange={this.handleChange}
+                      id={"message"}
+                      required={true}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <button className="button is-link" type="submit">
+                    Send
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </section>
+    );
+  }
 }
-
-export default LetsTalk;
-
-const Wrapper = styled.div`
-  width: 100%;
-  height: 100vh;
-`;
